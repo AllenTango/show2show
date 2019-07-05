cbbe56c1c7f5d598787ee4beca14b2513a881243
AllenTango <allentango@163.com>
2019-07-06 01:41:21 +0800

    提交 在 codecademy 上的作业

---
+++ ChoreDoor/script.js
@@ -0,0 +1,108 @@
+let doorImage1 = document.getElementById('door1');
+let doorImage2 = document.getElementById('door2');
+let doorImage3 = document.getElementById('door3');
+let startButton = document.getElementById('start');
+let currentlyPlaying = true;
+// 再次加载 路径是需要绝对路径么？ 2019-06-05
+let closedDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg';
+let botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
+let beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
+let spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
+let numClosedDoors = 3;
+let score = 0;
+let highScore = 0;
+let currentStreak = document.getElementById('score-number');
+let bestStreak = document.getElementById('high-score-number');
+let openDoor1, openDoor2, openDoor3;
+
+currentStreak.innerHTML = score;
+bestStreak.innerHTML = highScore;
+
+const randomChoreDoorGenerator = () => {
+  let choreDoor = Math.floor(Math.random() * 3);
+
+  switch (choreDoor) {
+    case 0:
+      openDoor1 = botDoorPath;
+      openDoor2 = spaceDoorPath;
+      openDoor3 = beachDoorPath;
+      break;
+    case 1:
+      openDoor1 = beachDoorPath;
+      openDoor2 = botDoorPath;
+      openDoor3 = spaceDoorPath;
+      break;
+    case 2:
+      openDoor1 = spaceDoorPath;
+      openDoor2 = beachDoorPath;
+      openDoor3 = botDoorPath;
+      break;
+  }
+};
+
+const playDoor = door => {
+  numClosedDoors--;
+  if (numClosedDoors === 0) {
+    gameOver('win');
+  } else if (isBot(door)){
+    gameOver('lose');
+    currentlyPlaying = false;
+  }
+};
+
+const isBot = door => door.src === botDoorPath ? true : false;
+
+const isClicked = door => door.src === closedDoorPath ? false : true;
+
+const startRound = () => {
+  doorImage1.src = closedDoorPath;
+  doorImage2.src = closedDoorPath;
+  doorImage3.src = closedDoorPath;
+  numClosedDoors = 3;
+  startButton.innerHTML = '好运哟！❤️';
+  currentlyPlaying = true;
+  randomChoreDoorGenerator();
+};
+
+const saveScore = () => {
+  score++;
+  currentStreak.innerHTML = score;
+  if (score > highScore) {
+    highScore = score;
+    bestStreak.innerHTML = highScore;
+  }
+};
+
+const gameOver = (status) => {
+  if (status === 'win') {
+    startButton.innerHTML = '你赢啦！再试一次？❤️';
+    saveScore();
+  } else {
+    startButton.innerHTML = '被扫出门啦，再试一次？🤔️';
+    score = 0;
+    currentStreak.innerHTML = score;
+  }
+  currentlyPlaying = false;
+};
+
+doorImage1.onclick = () => {
+  if (currentlyPlaying && !isClicked(doorImage1)) {
+    doorImage1.src = openDoor1;
+    playDoor(doorImage1);
+  }
+};
+doorImage2.onclick = () => {
+  if (currentlyPlaying && !isClicked(doorImage2)) {
+    doorImage2.src = openDoor2;
+    playDoor(doorImage2);
+  }
+};
+doorImage3.onclick = () => {
+  if (currentlyPlaying && !isClicked(doorImage3)) {
+    doorImage3.src = openDoor3;
+    playDoor(doorImage3);
+  }
+};
+
+startButton.onclick = startRound;
+startRound();
--- README.md
+++ README.md
@@ -10,4 +10,19 @@
 - 简单爬虫 (爬取GitHub octodex 图片)
 - [Ottergram (更炫酷的图片墙)](https://allentango.github.io/show2show/ottergram/index.html)
 - [初试小程序 dev](https://github.com/AllenTango/show2show/fungo)
-- [简单 WebComponent 实现todoApp](https://allentango.github.io/show2show/TodoApp/index.html)
+- [TodoApp 初了解 Web组件 shadowDOM](https://allentango.github.io/show2show/TodoApp/index.html)
+
+## CodeCademy 作业展示
+
+- [Wine Schedule 关于HTML tabels](https://allentango.github.io/show2show/WineSchedule/index.html)
+- [Excursion 利用CSS简单还原设计稿](https://allentango.github.io/show2show/ExcursionProject/index.html)
+- [Broadway 媒体查询](https://allentango.github.io/show2show/Broadway/index.html)
+- [Davie's Burgers](https://allentango.github.io/show2show/Davie'sBurgers/index.html)
+- [Tea Cozy 利用flex容器实现布局](https://allentango.github.io/show2show/TeaCozy/index.html)
+- [Colmar Academy 利用flex容器实现响应式网页](https://allentango.github.io/show2show/ColmarAcademy/index.html)
+- [Sole Shoes jQuery实现部分功能 >菜单展示 尺码选取](https://allentango.github.io/show2show/Sole/index.html)
+- [Chore Door 了解js 点击事件](https://allentango.github.io/show2show/ChoreDoor/index.html)
+- [PianoKeys](https://allentango.github.io/show2show/Pianokeys/index.html)
+- [Musicon 初了解 handlebars 模版语言](https://allentango.github.io/show2show/Musicon/index.html)
+- [word smith (利用 API 实现get 请求) Helper函数少少看得懂](https://allentango.github.io/show2show/WordSmith/index.html)
+- [Wanderlust 利用async fetch 实现异步请求](https://allentango.github.io/show2show/Wanderlust/index.html)
